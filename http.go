10885c7567b74bd6ddeb93e2758e750c6b2f4a42
Igor Zibarev <zibarev.i@gmail.com>
2019-03-21 18:35:30 +0300

    Add internal endpoints: healthcheck

--- http.go
+++ http.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"path"
+	"strings"
 )
 
 // ImportsHandler handles HTTP request that generates go-import template.
@@ -12,6 +14,17 @@ type ImportsHandler struct {
 
 // ServeHTTP implements http.Handler.
 func (h ImportsHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if strings.HasPrefix(strings.TrimPrefix(req.URL.Path, "/"), "_status") {
+		// Internal endpoints.
+		base := path.Base(strings.TrimSuffix(req.URL.Path, "/"))
+		if base == "healthz" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
 	h.Logger.Infof("Serve HTTP request: %s", req.RequestURI)
 
 	if err := h.Generator.Generate(w, req.URL.Path); err != nil {
